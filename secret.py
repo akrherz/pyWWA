46b9f4ba4c861c694f3540c248e33d1d8e224b08
akrherz <akrherz@iastate.edu>
2007-12-27 20:21:51 +0000

    necessary piece

---
+++ secret.py
@@ -0,0 +1,7 @@
+# Set these for your environment
+chatserver = 'localhost'
+connect_chatserver = 'localhost'
+iembot_ingest_password = 'password'
+dbhost = 'localhost'
+dbuser = 'akrherz'
+dbname = 'postgis'
