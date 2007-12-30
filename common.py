3e7625261466d742d9187944567eee8f2b919362
akrherz <akrherz@iastate.edu>
2007-12-30 22:44:38 +0000

    some cleanups before I chat about this

--- common.py
+++ common.py
@@ -15,15 +15,19 @@ class JabberClient:
     def authd(self,xmlstream):
         logging.info("Logged into Jabber Chat Server!")
         self.xmlstream = xmlstream
+        self.xmlstream.rawDataInFn = self.rawDataInFn
+        self.xmlstream.rawDataOutFn = self.rawDataOutFn
+
         presence = domish.Element(('jabber:client','presence'))
+        presence.addElement('status').addContent('Online')
         xmlstream.send(presence)
 
-        xmlstream.addObserver('/message',  self.debug)
-        xmlstream.addObserver('/presence', self.debug)
-        xmlstream.addObserver('/iq',       self.debug)
+        #xmlstream.addObserver('/message',  self.debug)
+        #xmlstream.addObserver('/presence', self.debug)
+        #xmlstream.addObserver('/iq',       self.debug)
 
     def sendMessage(self, body, html=None):
-        while (self.xmlstream is None):
+        if (self.xmlstream is None):
             logging.info("xmlstream is None, so lets try again!")
             reactor.callLater(3, self.sendMessage, body, html)
             return
@@ -35,10 +39,13 @@ class JabberClient:
         message.addElement('body',None,body)
         if (html is not None):
             message.addRawXml("<html xmlns='http://jabber.org/protocol/xhtml-im'><body xmlns='http://www.w3.org/1999/xhtml'>"+ html +"</body></html>")
-        self.debug( message )
         self.xmlstream.send(message)
 
 
     def debug(self, elem):
         logging.info( elem.toXml().encode('utf-8') )
-        logging.info("="*20 )
+
+    def rawDataInFn(self, data):
+        print 'RECV', unicode(data,'utf-8','ignore').encode('ascii', 'replace')
+    def rawDataOutFn(self, data):
+        print 'SEND', unicode(data,'utf-8','ignore').encode('ascii', 'replace')
