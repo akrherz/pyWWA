11cb5b2e5ed570f572097214e270345d1cbf6c7f
akrherz <akrherz@iastate.edu>
2008-02-07 19:36:38 +0000

    oopsy

--- secret.py
+++ secret.py
@@ -1,7 +1,7 @@
 # Set these for your environment
 chatserver = 'localhost'
 connect_chatserver = 'localhost'
-iembot_ingest_password = 'cyclogenesis'
+iembot_ingest_password = 'bogus'
 dbhost = 'localhost'
 dbuser = 'akrherz'
 dbname = 'postgis'
