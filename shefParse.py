faca89e3762c1c3c9951375b5a4e425e2ce0308e
akrherz <akrherz@iastate.edu>
2008-02-15 16:15:01 +0000

    better logging, license statement

--- shefParse.py
+++ shefParse.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2005 Iowa State University
+# http://mesonet.agron.iastate.edu/ -- mailto:akrherz@iastate.edu
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+""" SHEF product ingestor """
+
 
 import sys, sys, os
 import traceback
@@ -8,7 +25,10 @@ import smtplib, StringIO
 from email.MIMEText import MIMEText
 import secret
 
-log.startLogging( open('/tmp/shefParse.log','a') )
+log.startLogging(open('/mesonet/data/logs/%s/shefParse.log' \
+    % (os.getenv("USER"),), 'a'))
+log.FileLogObserver.timeFormat = "%Y/%m/%d %H:%M:%S %Z"
+
 
 found_vars = {}
 
