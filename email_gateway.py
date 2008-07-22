c5713c198d501dd84e7a2e1b4dcc8225b081b25e
akrherz <akrherz@iastate.edu>
2008-07-22 04:38:40 +0000

    add simplistic router :(

--- email_gateway.py
+++ email_gateway.py
@@ -6,10 +6,72 @@ import mimetypes, mx.DateTime
 import sys, os, datetime, re, StringIO, traceback, smtplib
 from email.MIMEText import MIMEText
 
-
 os.chdir("/home/ldm/pyWWA/")
 
+from twisted.words.protocols.jabber import client, jid
+from twisted.words.xish import domish
+from twisted.internet import reactor
+
+qu = 0
+class JabberClient:
+    xmlstream = None
+
+    def __init__(self, myJid):
+        self.myJid = myJid
+
+
+    def authd(self,xmlstream):
+        print "authenticated"
+        self.xmlstream = xmlstream
+        presence = domish.Element(('jabber:client','presence'))
+        xmlstream.send(presence)
+
+        xmlstream.addObserver('/message',  self.debug)
+        xmlstream.addObserver('/presence', self.debug)
+        xmlstream.addObserver('/iq',       self.debug)
+
+    def debug(self, elem):
+        print elem.toXml().encode('utf-8')
+        print "="*20
+
+    def sendMessage(self, body, htmlbody):
+        global qu
+        while (self.xmlstream is None):
+            print "xmlstream is None, so lets try again!"
+            reactor.callLater(3, self.sendMessage, body, htmlbody)
+            return
+
+        message = domish.Element(('jabber:client','message'))
+        message['to'] = "iembot@%s" % (secret.chatserver,)
+        message['type'] = 'chat'
+        message.addElement('body',None,body)
+
+        html = domish.Element(('http://jabber.org/protocol/xhtml-im','html'))
+        body = domish.Element(('http://www.w3.org/1999/xhtml','body'))
+        body.addRawXml( htmlbody )
+        html.addChild(body)
+        message.addChild(html)
+        self.xmlstream.send(message)
+        qu -= 1
+
+def process():
+  try:
+    process_message( sys.stdin )
+  except:
+    io = StringIO.StringIO()
+    traceback.print_exc(file=io)
+    msg = MIMEText("%s\n" % (io.getvalue(),) )
+    msg['subject'] = 'iembot gateway traceback'
+    msg['From'] = secret.parser_user
+    msg['To'] = "akrherz@iastate.edu"
+
+    s = smtplib.SMTP()
+    s.connect()
+    s.sendmail(msg["From"], msg["To"], msg.as_string())
+    s.close()
+
 def process_message( data ):
+  global qu
   # Read message, data supports .read()
   msg = email.message_from_file( data )
 
@@ -18,6 +80,11 @@ def process_message( data ):
   # akrherz+service@host
   service = msg['to'].split("@")[0].split("+")[1]
   pil = secret.srvlkp[service]['pil']
+  if (pil == "TBW-0"):
+    body = "TBW: Updated GraphiCast http://www.srh.noaa.gov/tbw/html/tbw/Graphicast.php"
+    htmlbody = "TBW has updated the <a href=\"http://www.srh.noaa.gov/tbw/html/tbw/Graphicast.php\">GraphiCast</a>"
+    jabber.sendMessage(body, htmlbody)
+    return
 
   ts = mx.DateTime.gmt().strftime("%d%H%M")
   edesc = "109 \nZZZZ63 K%s %s\n%s\n\n" % (pil[3:], ts, pil)
@@ -39,19 +106,29 @@ def process_message( data ):
   cmd = "/home/ldm/bin/pqinsert -f WMO -p '/p%s' %s" % (pil, fname)
   os.system(cmd)
 
-if (__name__ == "__main__"):
-  try:
-    process_message( sys.stdin )
-  except:
-    io = StringIO.StringIO()
-    traceback.print_exc(file=io)
-    msg = MIMEText("%s\n" % (io.getvalue(),) )
-    msg['subject'] = 'iembot gateway traceback'
-    msg['From'] = secret.parser_user
-    msg['To'] = "akrherz@iastate.edu"
+def killer():
+    global qu
+    print "queue", qu
+    if (qu == 0):
+        reactor.stop()
+    reactor.callLater(10, killer)
 
-    s = smtplib.SMTP()
-    s.connect()
-    s.sendmail(msg["From"], msg["To"], msg.as_string())
-    s.close()
+myJid = jid.JID('iembot_ingest@%s/ingestor_%s' % (secret.chatserver, mx.DateTime
+.now().ticks() ) )
+factory = client.basicClientFactory(myJid, secret.iembot_ingest_password)
+
+jabber = JabberClient(myJid)
+
+factory.addBootstrap('//event/stream/authd',jabber.authd)
+factory.addBootstrap("//event/client/basicauth/invaliduser", jabber.debug)
+factory.addBootstrap("//event/client/basicauth/authfailed", jabber.debug)
+factory.addBootstrap("//event/stream/error", jabber.debug)
+
+reactor.connectTCP(secret.chatserver,5222,factory)
+reactor.callLater(0, process)
+reactor.callLater(10, killer)
+reactor.run()
+
+
+
 
