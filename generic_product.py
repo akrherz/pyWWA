03d1abf7d2ea66d47e7a4e7c4029ad882e9ae7b8
akrherz <akrherz@iastate.edu>
2008-07-30 14:30:27 +0000

    reorder some things

--- generic_product.py
+++ generic_product.py
@@ -1,6 +1,29 @@
-# Generic Text product to iembot
+# Copyright (c) 2005 Iowa State University
+# http://mesonet.agron.iastate.edu/ -- mailto:akrherz@iastate.edu
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+""" generic product ingestor """
 
+__revision__ = '$Id: generic_product.py 3802 2008-07-29 19:55:56Z akrherz $'
+
+# Twisted Python imports
+from twisted.words.protocols.jabber import client, jid, xmlstream
+from twisted.internet import reactor
 from twisted.python import log
+from twisted.enterprise import adbapi
+
+
 import os
 log.startLogging(open('logs/gp.log', 'a'))
 log.FileLogObserver.timeFormat = "%Y/%m/%d %H:%M:%S %Z"
@@ -16,7 +39,6 @@ import smtplib
 import common
 from support import ldmbridge, TextProduct, reference
 import psycopg2
-from twisted.enterprise import adbapi
 
 DBPOOL = adbapi.ConnectionPool("psycopg2", database=secret.dbname, host=secret.dbhost, password=secret.dbpass)
 
@@ -34,8 +56,6 @@ routes = {'TCPAT[0-9]': gulfwfo,
           'DSAAT': gulfwfo,
           'SWODY[1-2]': spcwfo,}
 
-from twisted.words.protocols.jabber import client, jid
-from twisted.internet import reactor
 
 
 errors = StringIO.StringIO()
@@ -257,6 +277,7 @@ factory.addBootstrap('//event/stream/authd',jabber.authd)
 factory.addBootstrap("//event/client/basicauth/invaliduser", jabber.debug)
 factory.addBootstrap("//event/client/basicauth/authfailed", jabber.debug)
 factory.addBootstrap("//event/stream/error", jabber.debug)
+factory.addBootstrap(xmlstream.STREAM_END_EVENT, jabber._disconnect )
 
 reactor.connectTCP(secret.chatserver,5222,factory)
 
