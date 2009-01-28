a0d3061b2da9225e25cb0a374e017d27856499c8
akrherz <akrherz@iastate.edu>
2009-01-28 00:30:19 +0000

    yikes, make this actually work!

--- mcdParse.py
+++ mcdParse.py
@@ -25,81 +25,55 @@ import traceback, StringIO
 import smtplib
 from email.MIMEText import MIMEText
 import secret
-from support import TextProduct
+from support import TextProduct, ldmbridge
+import common
 
 import pg
 POSTGIS = pg.connect(secret.dbname, secret.dbhost, user=secret.dbuser, passwd=secret.dbpass)
 
 from twisted.words.protocols.jabber import client, jid
-from twisted.words.xish import domish
+from twisted.words.xish import domish, xmlstream
 from twisted.internet import reactor
+from twisted.mail import smtp
 
-raw = sys.stdin.read()
+EMAILS = 10
 
-qu = 0
+def email_error(message, product_text):
+    """
+    Generic something to send email error messages 
+    """
+    global EMAILS
+    log.msg( message )
+    EMAILS -= 1
+    if (EMAILS < 0):
+        return
 
-class JabberClient:
-    xmlstream = None
+    msg = MIMEText("Exception:\n%s\n\nRaw Product:\n%s" \
+                 % (message, product_text))
+    msg['subject'] = 'mcd_parser.py Traceback'
+    msg['From'] = secret.parser_user
+    msg['To'] = 'akrherz@iastate.edu'
+    smtp.sendmail("mailhub.iastate.edu", msg["From"], msg["To"], msg)
 
-    def __init__(self, myJid):
-        self.myJid = myJid
+# LDM Ingestor
+class MyProductIngestor(ldmbridge.LDMProductReceiver):
+    """ I receive products from ldmbridge and process them 1 by 1 :) """
 
+    def connectionLost(self, reason):
+        print 'connectionLost', reason
+        reactor.callLater(5, self.shutdown)
 
-    def authd(self,xmlstream):
-        print "authenticated"
-        self.xmlstream = xmlstream
-        presence = domish.Element(('jabber:client','presence'))
-        xmlstream.send(presence)
-
-        xmlstream.addObserver('/message',  self.debug)
-        xmlstream.addObserver('/presence', self.debug)
-        xmlstream.addObserver('/iq',       self.debug)
-
-    def debug(self, elem):
-        print elem.toXml().encode('utf-8')
-        print "="*20
-
-    def sendMessage(self, body, htmlbody):
-        global qu
-        while (self.xmlstream is None):
-            print "xmlstream is None, so lets try again!"
-            reactor.callLater(3, self.sendMessage, body, htmlbody)
-            return
-
-        message = domish.Element(('jabber:client','message'))
-        message['to'] = "%s@%s" % (secret.iembot_user, secret.chatserver)
-        message['type'] = 'chat'
-        message.addElement('body',None,body)
-
-        html = domish.Element(('http://jabber.org/protocol/xhtml-im','html'))
-        body = domish.Element(('http://www.w3.org/1999/xhtml','body'))
-        body.addRawXml( htmlbody )
-        html.addChild(body)
-        message.addChild(html)
-        self.xmlstream.send(message)
-        qu -= 1
+    def shutdown(self):
+        reactor.callWhenRunning(reactor.stop)
 
 
-
-def process(raw):
-    try:
-        real_process(raw)
-    except:
-        io = StringIO.StringIO()
-        traceback.print_exc(file=io)
-        msg = MIMEText("%s\n\n>RAW DATA\n\n%s"%(io.getvalue(),raw))
-        msg['subject'] = 'mcdParse.py Traceback'
-        msg['From'] = secret.parser_user
-        msg['To'] = "akrherz@iastate.edu"
-
-        s = smtplib.SMTP()
-        s.connect()
-        s.sendmail(msg["From"], msg["To"], msg.as_string())
-        s.close()
-
+    def process_data(self, raw):
+        try:
+            real_process(raw)
+        except Exception, myexp:
+            email_error(myexp, raw)
 
 def real_process(raw):
-    global qu
     tokens = re.findall("MESOSCALE DISCUSSION ([0-9]*)", raw)
     num = tokens[0]
 
@@ -115,37 +89,27 @@ def real_process(raw):
     tokens = re.findall("ATTN\.\.\.WFO\.\.\.([\.,A-Z]*)", raw)
     tokens = re.findall("([A-Z][A-Z][A-Z])", tokens[0])
     for wfo in tokens:
-        qu += 1
         body = "%s: Storm Prediction Center issues Mesoscale Discussion http://www.spc.noaa.gov/products/md/md%s.html" % \
          (wfo, num)
         htmlbody = "Storm Prediction Center issues <a href='http://www.spc.noaa.gov/products/md/md%s.html'>Mesoscale Discussion #%s</a> (<a href='%s?pid=%s'>View text</a>)" %(num,num, secret.PROD_URL, product_id)
-        print body
         jabber.sendMessage(body, htmlbody)
 
-
-def killer():
-    global qu
-    print "queue", qu
-    if (qu == 0):
-        reactor.stop()
-    reactor.callLater(10, killer)
 
 myJid = jid.JID('%s@%s/mcdparse_%s' % \
       (secret.iembot_ingest_user, secret.chatserver, \
        mx.DateTime.gmt().strftime("%Y%m%d%H%M%S") ) )
 factory = client.basicClientFactory(myJid, secret.iembot_ingest_password)
 
-jabber = JabberClient(myJid)
+jabber = common.JabberClient(myJid)
 
 factory.addBootstrap('//event/stream/authd',jabber.authd)
 factory.addBootstrap("//event/client/basicauth/invaliduser", jabber.debug)
 factory.addBootstrap("//event/client/basicauth/authfailed", jabber.debug)
 factory.addBootstrap("//event/stream/error", jabber.debug)
+factory.addBootstrap(xmlstream.STREAM_END_EVENT, jabber._disconnect )
 
 reactor.connectTCP(secret.connect_chatserver,5222,factory)
-reactor.callLater(0, process, raw)
-reactor.callLater(10, killer)
+
+ldm = ldmbridge.LDMProductFactory( MyProductIngestor() )
+
 reactor.run()
-
-
-
