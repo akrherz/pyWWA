0c74512159641fa63cf6292439a20c6af9698a02
akrherz <akrherz@iastate.edu>
2009-01-31 17:51:56 +0000

    add a template program so that we can base others off it :)

---
+++ template.py
@@ -0,0 +1,103 @@
+# Copyright (c) 2005 Iowa State University
+# http://mesonet.agron.iastate.edu/ -- mailto:akrherz@iastate.edu
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+""" Template Example ingestor """
+
+__revision__ = '$Id: template.py 4513 2009-01-06 16:57:49Z akrherz $'
+
+# Twisted Python imports
+from twisted.words.protocols.jabber import client, jid, xmlstream
+from twisted.internet import reactor
+from twisted.python import log
+from twisted.enterprise import adbapi
+from twisted.mail import smtp
+
+# Standard Python modules
+import os, re, traceback, StringIO, smtplib
+from email.MIMEText import MIMEText
+
+# Python 3rd Party Add-Ons
+import mx.DateTime, pg
+
+# pyWWA stuff
+from support import ldmbridge, TextProduct, reference
+import secret
+import common
+
+log.startLogging(open('logs/template.log','a'))
+log.FileLogObserver.timeFormat = "%Y/%m/%d %H:%M:%S %Z"
+
+POSTGIS = pg.connect(secret.dbname, secret.dbhost, user=secret.dbuser, 
+                     passwd=secret.dbpass)
+DBPOOL = adbapi.ConnectionPool("psycopg2", database=secret.dbname, 
+                               host=secret.dbhost, password=secret.dbpass)
+EMAILS = 10
+
+def email_error(message, product_text):
+    """
+    Generic something to send email error messages 
+    """
+    global EMAILS
+    log.msg( message )
+    EMAILS -= 1
+    if (EMAILS < 0):
+        return
+
+    msg = MIMEText("Exception:\n%s\n\nRaw Product:\n%s" \
+                 % (message, product_text))
+    msg['subject'] = 'template.py Traceback'
+    msg['From'] = secret.parser_user
+    msg['To'] = secret.error_user
+    smtp.sendmail("localhost", msg["From"], msg["To"], msg)
+
+# LDM Ingestor
+class MyProductIngestor(ldmbridge.LDMProductReceiver):
+    """ I receive products from ldmbridge and process them 1 by 1 :) """
+
+    def connectionLost(self, reason):
+        print 'connectionLost', reason
+        reactor.callLater(5, self.shutdown)
+
+    def shutdown(self):
+        reactor.callWhenRunning(reactor.stop)
+
+
+    def process_data(self, buf):
+        """ Process the product """
+        try:
+            real_parser(buf)
+        except Exception, myexp:
+            email_error(myexp, buf)
+
+def real_parser(buf):
+    jabber.sendMessage(buf)
+
+myJid = jid.JID('%s@%s/template_%s' % \
+      (secret.iembot_ingest_user, secret.chatserver, \
+       mx.DateTime.gmt().strftime("%Y%m%d%H%M%S") ) )
+factory = client.basicClientFactory(myJid, secret.iembot_ingest_password)
+
+jabber = common.JabberClient(myJid)
+
+factory.addBootstrap('//event/stream/authd', jabber.authd)
+factory.addBootstrap("//event/client/basicauth/invaliduser", jabber.debug)
+factory.addBootstrap("//event/client/basicauth/authfailed", jabber.debug)
+factory.addBootstrap("//event/stream/error", jabber.debug)
+factory.addBootstrap(xmlstream.STREAM_END_EVENT, jabber._disconnect )
+
+reactor.connectTCP(secret.connect_chatserver, 5222, factory)
+
+ldm = ldmbridge.LDMProductFactory( MyProductIngestor() )
+reactor.run()
