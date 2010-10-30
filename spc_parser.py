b358536a75dce0357eaf4d665cff44d5f956eba6
akrherz <akrherz@iastate.edu>
2010-10-30 20:37:53 +0000

    intermediate commit

--- spc_parser.py
+++ spc_parser.py
@@ -32,7 +32,7 @@ from email.MIMEText import MIMEText
 import mx.DateTime, pg
 
 # pyWWA stuff
-from support import ldmbridge, TextProduct, reference
+from support import ldmbridge, TextProduct, reference, spcpts
 import secret
 import common
 
@@ -74,170 +74,107 @@ class MyProductIngestor(ldmbridge.LDMProductReceiver):
         except Exception, myexp:
             common.email_error(myexp, buf)
 
-def consumer(category, metadata, lons, lats):
+def consume(spc, outlook):
     """
     Do the geometric stuff we care about, including storage
     """
-    # Here is a hack to account for when there is only two points
-    # in the polygon.
-    if len(lons) == 2:
-        lons.append( lons[-1] + 0.01 )
-        lats.append( lats[-1] + 0.01 )
-    if lons[0] != lons[-1]:
-        lons.append( lons[0] )
-        lats.append( lats[0] )
-    geometry = "MULTIPOLYGON((( "
-    for i in range(len(lons)):
-        geometry += "%.2f %.2f," % (lons[i], lats[i])
-    geometry = geometry[:-1] + " )))"
+    
     # Insert geometry into database table please
     sql = """INSERT into spc_outlooks(issue, valid, expire,
-        category, category_type, day, outlook_type, geom)
+        threshold, category, day, outlook_type, geom)
     VALUES ('%s+00', '%s+00', '%s+00', '%s', '%s', '%s', '%s', 
     'SRID=4326;%s')""" % (
-            metadata['issue'], metadata['valid'], metadata['expire'],
-            category, metadata['category_type'], metadata['day'], 
-            metadata['outlook_type'], geometry)
+            spc.issue,  spc.valid, spc.expire,
+            outlook.threshold, outlook.category, spc.day, 
+            spc.outlook_type, outlook.polygon.wkt)
     DBPOOL.runOperation( sql ).addErrback(common.email_error, sql)
     
     # Search for WFOs
-    sql = "select distinct wfo from nws_ugc \
-       WHERE ( st_overlaps(geomFromEWKT('SRID=4326;%s'), geom) or \
-       st_contains(geomFromEWKT('SRID=4326;%s'), geom) )and \
-       polygon_class = 'C'" % \
-       (geometry, geometry)
+    sql = """select distinct wfo from nws_ugc 
+       WHERE ( st_overlaps(geomFromEWKT('SRID=4326;%s'), geom) or 
+       st_contains(geomFromEWKT('SRID=4326;%s'), geom) )and 
+       polygon_class = 'C'""" % (outlook.polygon.wkt, outlook.polygon.wkt)
 
     rs = POSTGIS.query(sql).dictresult()
     affectedWFOS = []
     for i in range(len(rs)):
         affectedWFOS.append( rs[i]['wfo'] )
 
-    print "Category Type: %s Category: %s #Lons: %s #Lats: %s #WFOS: %s" % (
-        metadata['category_type'], category, len(lons), len(lats), 
-        len(affectedWFOS))
+    print "Category: %s Threshold: %s  #WFOS: %s" % (
+        outlook.category, outlook.threshold,  len(affectedWFOS))
     return affectedWFOS
 
-def find_issue_expire(buf, tp):
-    """
-    Determine the period this product is valid for
-    """
-    tokens = re.findall("VALID TIME ([0-9]{6})Z - ([0-9]{6})Z", buf)
-    day1 = int(tokens[0][0][:2])
-    hour1 = int(tokens[0][0][2:4])
-    min1 = int(tokens[0][0][4:])
-    day2 = int(tokens[0][1][:2])
-    hour2 = int(tokens[0][1][2:4])
-    min2 = int(tokens[0][1][4:])
-    valid = tp.issueTime + mx.DateTime.RelativeDateTime(day=day1,hour=hour1,minute=min1)
-    expire = tp.issueTime + mx.DateTime.RelativeDateTime(day=day2,hour=hour2,minute=min2)
-    if day1 < tp.issueTime.day and day1 == 1:
-        valid += mx.DateTime.RelativeDateTime(months=1)
-    if day2 < tp.issueTime.day and day2 == 1:
-        expire += mx.DateTime.RelativeDateTime(months=1)
-    return valid, expire
+
 
 def real_parser(buf):
     buf = buf.replace("\015\015\012", "\n")
     tp = TextProduct.TextProduct(buf)
-    valid, expire = find_issue_expire(buf, tp)
-
-    metadata = {'valid': valid, 'expire': expire, 'issue': tp.issueTime}
-
-    if tp.afos == "PTSDY1":
-        metadata['day'] = '1'
-        metadata['outlook_type'] = 'C'
-        channelprefix = ""
-        product_descript = "Day 1 Convective"
-        url = "http://www.spc.noaa.gov/products/outlook/archive/%s/day1otlk_%s.html" % (valid.year, valid.strftime("%Y%m%d_%H%M") )
-    if tp.afos == "PTSDY2":
-        metadata['day'] = '2'
-        metadata['outlook_type'] = 'C'
-        channelprefix = ""
-        product_descript = "Day 2 Convective"
-        url = "http://www.spc.noaa.gov/products/outlook/archive/%s/day2otlk_%s.html" % (valid.year, valid.strftime("%Y%m%d_%H%M") )
-    if tp.afos == "PTSDY3":
-        metadata['day'] = '3'
-        metadata['outlook_type'] = 'C'
-        channelprefix = ""
-        product_descript = "Day 3 Convective"
-        url = "http://www.spc.noaa.gov/products/outlook/archive/%s/day3otlk_%s.html" % (valid.year, valid.strftime("%Y%m%d_%H%M") )
-    if tp.afos == "PTSD48":
-        metadata['day'] = '4'
-        metadata['outlook_type'] = 'C'
-        channelprefix = ""
-        product_descript = "Day 4-8 Convective"
-        url = "http://www.spc.noaa.gov/products/exper/day4-8/archive/%s/day4-8_%s.html" % (valid.year, valid.strftime("%Y%m%d_%H%M") )
-    if tp.afos == "PFWFD1":
-        metadata['day'] = '1'
-        metadata['outlook_type'] = 'F'
-        channelprefix = "DAY1FWX"
-        product_descript = "Day 1 Fire"
-        url = "http://www.spc.noaa.gov/products/fire_wx/%s/%s.html" % (valid.year, valid.strftime("%y%m%d") )
-    if tp.afos == "PFWFD2":
-        metadata['day'] = '2'
-        metadata['outlook_type'] = 'F'
-        channelprefix = "DAY2FWX"
-        product_descript = "Day 2 Fire"
-        url = "http://www.spc.noaa.gov/products/fire_wx/%s/%s.html" % (valid.year, valid.strftime("%y%m%d") )
-    if tp.afos == "PFWF38":
-        metadata['day'] = '3'
-        metadata['outlook_type'] = 'F'
-        channelprefix = "DAY3FWX"
-        product_descript = "Day 3-8 Fire"
-        url = "http://www.spc.noaa.gov/products/fire_wx/%s/%s.html" % (valid.year, valid.strftime("%y%m%d") )
+    spc = spcpts.SPCPTS( tp )
+    spc.draw_outlooks()
+    return
 
     # Remove any previous data
-    POSTGIS.query("""DELETE from spc_outlooks where valid = '%(valid)s+00' 
-    and expire = '%(expire)s+00' 
-    and outlook_type = '%(outlook_type)s' and day = '%(day)s'""" % metadata)
+    POSTGIS.query("""DELETE from spc_outlooks where valid = '%s+00' 
+    and expire = '%s+00' 
+    and outlook_type = '%s' and day = '%s'""" % (
+                    spc.valid, spc.expire, spc.outlook_type, spc.day))
 
     wfos = {'TSTM': [], 'EXTM': [], 'SLGT': [], 'CRIT': [], 'MDT': [], 
             'HIGH': [] }
 
-    # First we split the product by &&
-    for segment in buf.split("&&")[:-1]:
-        # We need to figure out the probabilistic or category
-        tokens = re.findall("\.\.\. (.*) \.\.\.", segment)
-        metadata['category_type'] = tokens[0][0]
-        # Now we loop over the lines looking for data
-        category = None
-        for line in segment.split("\n"):
-            if re.match("^(D[3-8]|EXTM|SLGT|MDT|HIGH|CRIT|TSTM|0\.[0-9][0-9]) ", line) is not None:
-                if category is not None:
-                    awfos = consumer(category, metadata, lons, lats)
-                    if wfos.has_key( category ):
-                        wfos[category].append( awfos )
-                category = line.split()[0]
-                lats = []
-                lons = []
-            if category is None:
-                continue
-            tokens = re.findall("([0-9]{8})", line)
-            for t in tokens:
-                # new segment
-                if t == "99999999":
-                    awfos = consumer(category, metadata, lons, lats)
-                    if wfos.has_key( category ):
-                        wfos[category].append( awfos )
-                    lats = []
-                    lons = []
-                    continue
-                lats.append( float(t[:4]) / 100.0 )
-                lon = float(t[4:]) / 100.0
-                if int(t[4]) < 3:
-                    lons.append( -1.0 * (lon + 100.0) )
-                else:
-                    lons.append( -1.0 * lon )
-        if category is not None:
-            awfos = consumer(category, metadata, lons, lats)
-            if wfos.has_key( category ):
-                wfos[category].append( awfos )
+    for outlook in spc.outlooks:
+        arWFO = consume(spc, outlook)
+        if wfos.has_key( outlook.threshold ):
+            wfos[ outlook.threshold ].append( arWFO )
+    
+    if tp.afos == "PTSDY1":
+        channelprefix = ""
+        product_descript = "Day 1 Convective"
+        url = "http://www.spc.noaa.gov/products/outlook/archive/%s/day1otlk_%s.html" % (
+                                                    spc.valid.year, 
+                                                    spc.valid.strftime("%Y%m%d_%H%M") )
+    if tp.afos == "PTSDY2":
+        channelprefix = ""
+        product_descript = "Day 2 Convective"
+        url = "http://www.spc.noaa.gov/products/outlook/archive/%s/day2otlk_%s.html" % (
+                                            spc.valid.year, 
+                                                    spc.valid.strftime("%Y%m%d_%H%M") )
+    if tp.afos == "PTSDY3":
+        channelprefix = ""
+        product_descript = "Day 3 Convective"
+        url = "http://www.spc.noaa.gov/products/outlook/archive/%s/day3otlk_%s.html" % (
+                                                    spc.valid.year, 
+                                                    spc.valid.strftime("%Y%m%d_%H%M") )
+    if tp.afos == "PTSD48":
+        channelprefix = ""
+        product_descript = "Day 4-8 Convective"
+        url = "http://www.spc.noaa.gov/products/exper/day4-8/archive/%s/day4-8_%s.html" % (
+                                                    spc.valid.year, 
+                                                    spc.valid.strftime("%Y%m%d_%H%M") )
+    if tp.afos == "PFWFD1":
+        channelprefix = "DAY1FWX"
+        product_descript = "Day 1 Fire"
+        url = "http://www.spc.noaa.gov/products/fire_wx/%s/%s.html" % (spc.valid.year, 
+                                                    spc.valid.strftime("%Y%m%d") )
+    if tp.afos == "PFWFD2":
+        channelprefix = "DAY2FWX"
+        product_descript = "Day 2 Fire"
+        url = "http://www.spc.noaa.gov/products/fire_wx/%s/%s.html" % (spc.valid.year, 
+                                                    spc.valid.strftime("%Y%m%d") )
+    if tp.afos == "PFWF38":
+        channelprefix = "DAY3FWX"
+        product_descript = "Day 3-8 Fire"
+        url = "http://www.spc.noaa.gov/products/fire_wx/%s/%s.html" % (spc.valid.year, 
+                                                    spc.valid.strftime("%Y%m%d") )
+
+
 
     # Only tweet or Jabber for Day1, for now
     if tp.afos not in ('PTSDY1','PFWFD1'):
         return
     
-    codes = {'SLGT': "Slight", 'MDT': "Moderate", 'HIGH': 'High', 'CRIT': 'Critical', 'EXTM': 'Extreme'}
+    codes = {'SLGT': "Slight", 'MDT': "Moderate", 'HIGH': 'High', 
+             'CRIT': 'Critical', 'EXTM': 'Extreme'}
 
 
     msgs = {}
