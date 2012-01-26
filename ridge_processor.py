cc313ce28ba4be7cd6a0abadfdb7bfc591230aa2
akrherz <akrherz@iastate.edu>
2012-01-26 23:57:24 +0000

    add vcp to metadata

--- ridge_processor.py
+++ ridge_processor.py
@@ -37,7 +37,7 @@ def generate_image(message):
     siteID = message.getStringProperty("siteID")
     ticks = message.getLongProperty("validTime")
     productID = message.getStringProperty("productID")
-    #vcp = message.getStringProperty("vcp")
+    vcp = message.getStringProperty("vcp")
     upperLeftLat = float(message.getStringProperty("upperLeftLat"))
     upperLeftLon = float(message.getStringProperty("upperLeftLon"))
     lowerRightLat = float(message.getStringProperty("lowerRightLat"))
@@ -53,6 +53,7 @@ def generate_image(message):
     metadata['meta']['valid'] = gts.strftime("%Y-%m-%dT%H:%M:%SZ")
     metadata['meta']['product'] = productID
     metadata['meta']['site'] = siteID
+    metadata['meta']['vcp'] = vcp
     pqstr = "gis %s %s gis/images/4326/ridge/%s/%s_0.png GIS/ridge/%s/%s/%s_%s_%s.png png" % (
      routes, gts.strftime("%Y%m%d%H%M"), siteID, productID, 
      siteID, productID, siteID, productID, gts.strftime("%Y%m%d%H%M"))
