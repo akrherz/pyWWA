1e88e55362e6783b16e858b0f6592f6c8a93f8a9
akrherz <akrherz@iastate.edu>
2012-03-09 16:49:16 +0000

    email 401 errors

--- common.py
+++ common.py
@@ -156,6 +156,8 @@ def twitterErrback(error, tuser, channel, twt):
     log.msg("TWEET ERROR User: %s TWT: [%s] RES: %s" % (tuser, twt, error) )
     log.msg( error.getErrorMessage() )
     log.msg( error.value.response )
+    if error.value.status == 401:
+        email_error(error, "Unauthorized 401 for %s" % (tuser,))
 
 
 class JabberClient:
