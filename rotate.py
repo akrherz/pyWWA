7d5be47e54b2f8d6b05e74507b5fffa0f682e321
akrherz <akrherz@iastate.edu>
2013-01-11 14:22:20 -0600

    less hacky

--- rotate.py
+++ rotate.py
@@ -5,36 +5,38 @@ import sys
 import os
 import gzip
 
+BASE = "/home/ldm/data/"
+
 if __name__ == '__main__':
     # blah/file_
     data = sys.stdin.read()
     fnbase = sys.argv[1]
     fmt = sys.argv[2]
 
-    dirname = "/home/ldm/data/" +  "/".join( fnbase.split("/")[:-1] )
+    dirname =  "%s/%s" % (BASE, os.path.dirname(fnbase))
     if not os.path.isdir(dirname):
         os.makedirs(dirname)
 
     if fmt == "tif.Z":
         for i in range(9,-1,-1):
-            oldfp = "/home/ldm/data/%s%s.%s" % (fnbase,i,fmt)
-            newfp = "/home/ldm/data/%s%s.%s" % (fnbase,i+1,fmt)
+            oldfp = "%s/%s%s.%s" % (BASE, fnbase,i,fmt)
+            newfp = "%s/%s%s.%s" % (BASE, fnbase,i+1,fmt)
             if os.path.isfile(oldfp):
                 os.rename(oldfp, newfp)
 
-        o = open("/home/ldm/data/%s%s.%s" % (fnbase,0,fmt), 'wb')
+        o = open("%s/%s%s.%s" % (BASE, fnbase,0,fmt), 'wb')
         o.write(data)
         o.close()
         
-        data = gzip.open("/home/ldm/data/%s%s.%s" % (fnbase,0,fmt), 'rb').read()
+        data = gzip.open("%s/%s%s.%s" % (BASE, fnbase,0,fmt), 'rb').read()
         fmt = "tif"
 
     for i in range(9,-1,-1):
-        oldfp = "/home/ldm/data/%s%s.%s" % (fnbase,i,fmt)
-        newfp = "/home/ldm/data/%s%s.%s" % (fnbase,i+1,fmt)
+        oldfp = "%s/%s%s.%s" % (BASE, fnbase, i, fmt)
+        newfp = "%s/%s%s.%s" % (BASE, fnbase, i+1, fmt)
         if os.path.isfile(oldfp):
             os.rename(oldfp, newfp)
 
-    o = open("/home/ldm/data/%s%s.%s" % (fnbase,0,fmt), 'wb')
+    o = open("%s/%s%s.%s" % (BASE, fnbase,0,fmt), 'wb')
     o.write(data)
     o.close()
