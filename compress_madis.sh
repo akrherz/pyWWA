dfec5e90f570a9847d44da732c38c931a4121271
akrherz <akrherz@iastate.edu>
2013-04-08 13:30:20 -0500

    move files to longterm storage as well

--- compress_madis.sh
+++ compress_madis.sh
@@ -2,10 +2,16 @@
 # Something to compress the MADIS data, run from crontab_iem21.txt
 
 YYYYMMDD=$(date --date '1 day ago' +'%Y%m%d')
+YYYY=$(date --date '1 day ago' +'%Y')
 
 cd /mesonet/data/madis/metar
 gzip ${YYYYMMDD}*nc
+mkdir -p /mnt/longterm2/metar/${YYYY}
+mv ${YYYYMMDD}*nc.gz /mnt/longterm2/metar/${YYYY}
+
 cd /mesonet/data/madis/mesonet
 gzip ${YYYYMMDD}*nc
+mkdir -p /mnt/longterm2/mesonet/${YYYY}
+mv ${YYYYMMDD}*nc.gz /mnt/longterm2/mesonet/${YYYY}
 
 #END
