f9adc569517ab5895472ee2b241fa2150fbc2159
akrherz <akrherz@iastate.edu>
2013-12-12 13:09:14 -0600

    update to support psycopg2 2.5, appears to be backwards compat, fixes #7

--- twistedpg.py
+++ twistedpg.py
@@ -5,14 +5,14 @@ http://twistedmatrix.com/pipermail/twisted-python/2006-April/012955.html
 """
 
 from psycopg2 import *
-from psycopg2 import _psycopg as _2psycopg
+from psycopg2 import connect as _2connect
 from psycopg2.extensions import connection as _2connection
 from psycopg2.extras import RealDictCursor
 
 del connect
 def connect(*args, **kwargs):
     kwargs['connection_factory'] = connection
-    return _2psycopg.connect(*args, **kwargs)
+    return _2connect(*args, **kwargs)
 
 class connection(_2connection):
     def cursor(self):
