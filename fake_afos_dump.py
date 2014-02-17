1f41367e69155f2c49d2dbe23bc1873198af9b6e
akrherz <akrherz@iastate.edu>
2014-02-17 09:36:31 -0600

    update logic and save source, wmo to database

--- fake_afos_dump.py
+++ fake_afos_dump.py
@@ -2,16 +2,25 @@
   Dump some stuff without AFOS PILs
 """
 
-import iemdb, sys
-AFOS = iemdb.connect('afos')
+import psycopg2, sys
+from pyiem.nws.product import TextProduct
+AFOS = psycopg2.connect(database='afos', host='iemdb')
 acursor = AFOS.cursor()
 
 def main():
+    ''' We are called with a hard coded AFOS PIL '''
     pil = sys.argv[1]
-    myData = sys.stdin.read()
-
-    sql =  "INSERT into products (pil,data) VALUES (%s, %s)"
-    sqlargs = (pil,myData)
+    tp = TextProduct( sys.stdin.read() )
+    utc = tp.valid
+    table = "products_%s_0106" % (utc.year,)
+    if utc.month > 6:
+        table = "products_%s_0712" % (utc.year,)
+        
+    sql = """INSERT into """+table+"""(pil, data, source, wmo, entered) 
+        values(%s,%s,%s,%s,%s)"""
+    
+    sqlargs = (pil, tp.text,
+               tp.source, tp.wmo, utc.strftime("%Y-%m-%d %H:%M+00") )
     acursor.execute(sql, sqlargs)
 
 if __name__ == '__main__':
