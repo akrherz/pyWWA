9b8f88a45af34e3d31a2632ad4d1b48eb5aeec2b
akrherz <akrherz@iastate.edu>
2014-12-01 12:25:40 -0600

    revamp to use pyIEM v0.2 CLI parser

--- parsers/cli_parser.py
+++ parsers/cli_parser.py
@@ -32,14 +32,13 @@ class MyProductIngestor(ldmbridge.LDMProductReceiver):
 
     def process_data(self, text):
         """ Process the product """
-        deffer = DBPOOL.runInteraction(realparser, text)
+        deffer = DBPOOL.runInteraction(preprocessor, text)
         deffer.addErrback(common.email_error, text)
 
-def save_data(txn, prod):
+def save_data(txn, prod, station, data):
     """ Save atomic data to cli_data table """
-    # hopefully this prevents issues with ID conflicts and makes it easier
-    # to match with ASOS sites
-    station = "%s%s" % (prod.source[0], prod.afos[3:])
+    # Use four char here
+    station = "%s%s" % (prod.source[0], station)
 
     if not NT.sts.has_key(station):
         common.email_error("Unknown CLI Station: %s" % (station,),
@@ -47,7 +46,7 @@ def save_data(txn, prod):
 
     txn.execute("""
     SELECT product from cli_data where station = %s and valid = %s
-    """, (station, prod.cli_valid))
+    """, (station, data['cli_valid']))
     if txn.rowcount == 1:
         row = txn.fetchone()
         if prod.get_product_id() < row['product']:
@@ -55,7 +54,7 @@ def save_data(txn, prod):
                                     prod.get_product_id(), row['product'])
             return
         txn.execute("""DELETE from cli_data WHERE station = %s and valid = %s
-        """, (station, prod.cli_valid))
+        """, (station, data['cli_valid']))
 
     txn.execute("""INSERT into cli_data(
         station, product, valid, high, high_normal, high_record,
@@ -65,7 +64,8 @@ def save_data(txn, prod):
         precip_record_years, precip_month_normal, snow, snow_month,
         snow_jun1, snow_jul1, 
         snow_dec1, precip_dec1, precip_dec1_normal, precip_jan1_normal,
-        high_time, low_time, snow_record_years, snow_record) 
+        high_time, low_time, snow_record_years, snow_record,
+        snow_jun1_normal, snow_jul1_normal, snow_dec1_normal) 
         VALUES (
         %s, %s, %s, %s, %s, %s,
         %s, %s, %s, %s, %s,
@@ -74,33 +74,37 @@ def save_data(txn, prod):
         %s, %s, %s, %s,
         %s, %s,
         %s, %s, %s, %s,
-        %s, %s, %s, %s
+        %s, %s, %s, %s,
+        %s, %s, %s
         )
-    """, (station, prod.get_product_id(), prod.cli_valid,
-          prod.data.get('temperature_maximum'),
-          prod.data.get('temperature_maximum_normal'),
-          prod.data.get('temperature_maximum_record'),
-          prod.data.get('temperature_maximum_record_years', []),
-          prod.data.get('temperature_minimum'),
-          prod.data.get('temperature_minimum_normal'),
-          prod.data.get('temperature_minimum_record'),
-          prod.data.get('temperature_minimum_record_years', []),
-          prod.data.get('precip_today'),
-          prod.data.get('precip_month'),
-          prod.data.get('precip_jan1'), prod.data.get('precip_jul1'),
-          prod.data.get('precip_today_normal'),
-          prod.data.get('precip_today_record'),
-          prod.data.get('precip_today_record_years', []),
-          prod.data.get('precip_month_normal'),
-          prod.data.get('snow_today'), prod.data.get('snow_month'),
-          prod.data.get('snow_jun1'), prod.data.get('snow_jul1'),
-          prod.data.get('snow_dec1'), prod.data.get('precip_dec1'),
-          prod.data.get('precip_dec1_normal'),
-          prod.data.get('precip_jan1_normal'),
-          prod.data.get('temperature_maximum_time'),
-          prod.data.get('temperature_minimum_time'),
-          prod.data.get('snow_today_record_years', []),
-          prod.data.get('snow_today_record')
+    """, (station, prod.get_product_id(), data['cli_valid'],
+          data['data'].get('temperature_maximum'),
+          data['data'].get('temperature_maximum_normal'),
+          data['data'].get('temperature_maximum_record'),
+          data['data'].get('temperature_maximum_record_years', []),
+          data['data'].get('temperature_minimum'),
+          data['data'].get('temperature_minimum_normal'),
+          data['data'].get('temperature_minimum_record'),
+          data['data'].get('temperature_minimum_record_years', []),
+          data['data'].get('precip_today'),
+          data['data'].get('precip_month'),
+          data['data'].get('precip_jan1'), data['data'].get('precip_jul1'),
+          data['data'].get('precip_today_normal'),
+          data['data'].get('precip_today_record'),
+          data['data'].get('precip_today_record_years', []),
+          data['data'].get('precip_month_normal'),
+          data['data'].get('snow_today'), data['data'].get('snow_month'),
+          data['data'].get('snow_jun1'), data['data'].get('snow_jul1'),
+          data['data'].get('snow_dec1'), data['data'].get('precip_dec1'),
+          data['data'].get('precip_dec1_normal'),
+          data['data'].get('precip_jan1_normal'),
+          data['data'].get('temperature_maximum_time'),
+          data['data'].get('temperature_minimum_time'),
+          data['data'].get('snow_today_record_years', []),
+          data['data'].get('snow_today_record'),
+          data['data'].get('snow_jun1_normal'),
+          data['data'].get('snow_jul1_normal'),
+          data['data'].get('snow_dec1_normal')
           ))
 
 def send_tweet(prod):
@@ -108,54 +112,71 @@ def send_tweet(prod):
 
     jres = prod.get_jabbers(    
         common.settings.get('pywwa_product_url', 'pywwa_product_url'))
-    jabber.sendMessage(jres[0][0], jres[0][1], jres[0][2])
+    for j in jres:
+        jabber.sendMessage(j[0], j[1], j[2])
 
-
-
-def realparser(txn, text):
-    """ Actually do the work """
+def preprocessor(txn, text):
+    """ Protect the realprocessor """
     prod = parser(text)
-    if prod.data is None or prod.cli_valid is None:
+    if len(prod.data) == 0:
         return
+    for data in prod.data:
+        realprocessor(txn, prod, data)
+    send_tweet(prod)
 
-    station = prod.afos[3:]
-    table = "summary_%s" % (prod.cli_valid.year,)
+def realprocessor(txn, prod, data):
+    """ Actually do the work """
+    # Can't always use the AFOS as the station ID :(
+    if len(prod.data) > 1:
+        station = None
+        for stid in NT.sts.keys():
+            if NT.sts[stid]['name'].upper() == data['cli_station']:
+                station = stid[1:] # drop first char
+                break
+        if station is None:
+            common.email_error("Unknown CLI Station Text: |%s|" % (
+                                                data['cli_station'],), 
+                               prod.unixtext)
+            return
+    else:
+        station = prod.afos[3:]
+    table = "summary_%s" % (data['cli_valid'].year,)
     txn.execute("""
         SELECT max_tmpf, min_tmpf, pday, pmonth, snow from """+table+""" d
         JOIN stations t on (t.iemid = d.iemid)
         WHERE d.day = %s and t.id = %s and t.network ~* 'ASOS'
-        """, (prod.cli_valid, station))
+        """, (data['cli_valid'], station))
     row = txn.fetchone()
     if row is None:
-        print 'No %s rows found for %s on %s' % (table, station, prod.cli_valid)
-        save_data(txn, prod)
-        send_tweet(prod)
+        print 'No %s rows found for %s on %s' % (table, station, 
+                                                 data['cli_valid'])
+        save_data(txn, prod, station, data)
         return
     updatesql = []
     logmsg = []
-    if prod.data.get('temperature_maximum'):
-        climax = prod.data['temperature_maximum']
+    if data['data'].get('temperature_maximum'):
+        climax = data['data']['temperature_maximum']
         if int(climax) != row['max_tmpf']:
             updatesql.append(' max_tmpf = %s' % (climax,))
             logmsg.append('MaxT O:%s N:%s' % (row['max_tmpf'], climax))
-    if prod.data.get('temperature_minimum'):
-        climin = prod.data['temperature_minimum']
+    if data['data'].get('temperature_minimum'):
+        climin = data['data']['temperature_minimum']
         if int(climin) != row['min_tmpf']:
             updatesql.append(' min_tmpf = %s' % (climin,))
             logmsg.append('MinT O:%s N:%s' % (row['min_tmpf'], climin))
-    if prod.data.get('precip_month'):
-        val = prod.data['precip_month']
+    if data['data'].get('precip_month'):
+        val = data['data']['precip_month']
         if val != row['pmonth']:
             updatesql.append(' pmonth = %s' % (val,))
             logmsg.append('PMonth O:%s N:%s' % (row['pmonth'], val))
-    if prod.data.get('precip_today'):
-        val = prod.data['precip_today']
+    if data['data'].get('precip_today'):
+        val = data['data']['precip_today']
         if val != row['pday']:
             updatesql.append(' pday = %s' % (val,))
             logmsg.append('PDay O:%s N:%s' % (row['pday'], val))
 
-    if prod.data.get('snow_today'):
-        val = prod.data['snow_today']
+    if data['data'].get('snow_today'):
+        val = data['data']['snow_today']
         if row['snow'] is None or val != row['snow']:
             updatesql.append(' snow = %s' % (val,))
             logmsg.append('Snow O:%s N:%s' % (row['snow'], val))
@@ -169,8 +190,7 @@ def realparser(txn, text):
                                     prod.cli_valid.strftime("%y%m%d"),
                                     ','.join(logmsg)))
 
-    save_data(txn, prod)
-    send_tweet(prod)
+    save_data(txn, prod, station, data)
 
 
 if __name__ == '__main__':
