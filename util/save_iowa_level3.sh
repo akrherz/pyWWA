1741b1e8e311d492d6667eb0b835eedf7c780280
akrherz <akrherz@iastate.edu>
2015-01-19 14:31:46 -0600

    move scripts into util/

---
+++ util/save_iowa_level3.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Simple Script that will save RADAR data
+# Run from cron
+#set -x
+
+DIR=$(date -u -d "1 day ago" +'%Y_%m')
+YYYYMMDD=$(date -u -d "1 day ago" +'%Y%m%d')
+
+if [ ! -d /mesonet/ARCHIVE/nexrad/${DIR} ] 
+then
+	mkdir -p /mesonet/ARCHIVE/nexrad/${DIR}
+fi
+
+for R in $(echo "DMX DVN OAX ARX FSD MPX EAX ABR UDX"); do
+cd /mesonet/data/nexrad/NIDS/${R}
+  tar -cf /tmp/${R}radar.tar ?[A-Z]?/???_${YYYYMMDD}_*
+  tar -uf /tmp/${R}radar.tar ?[0-9]?/???_${YYYYMMDD}_*
+  cd /tmp
+  gzip -c ${R}radar.tar > ${R}radar.tgz
+  mv ${R}radar.tgz /mesonet/ARCHIVE/nexrad/${DIR}/${R}_${YYYYMMDD}.tgz
+  rm ${R}radar.tar
+done
+
+#END
