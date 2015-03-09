ee3095f234e4d1f9fe2e4e2ed4c126be0b0ee2f9
akrherz <akrherz@iastate.edu>
2015-03-09 15:59:06 -0500

    cleanup SPE parser, make it long running (for now)

--- parsers/spe_parser.py
+++ parsers/spe_parser.py
@@ -1,57 +1,76 @@
-"""SPENES product ingestor
-
-I am not longterm running, but exec once per product!
-"""
+"""SPENES product ingestor"""
 # Twisted Python imports
 from syslog import LOG_LOCAL2
 from twisted.python import syslog
+from twisted.python import log
 syslog.startLogging(prefix='pyWWA/spe_parser', facility=LOG_LOCAL2)
 from twisted.internet import reactor
 
-import sys
 import re
 import common
+# pyLDM https://github.com/akrherz/pyLDM
+from pyldm import ldmbridge
+
 
 from pyiem.nws import product
 POSTGIS = common.get_database('postgis', cp_max=1)
-raw = sys.stdin.read()
 PYWWA_PRODUCT_URL = common.settings.get('pywwa_product_url',
                                         'pywwa_product_url')
+DB_ON = bool(common.settings.get('pywwa_save_text_products', False))
 
 
-def process(raw):
-    try:
-        real_process(raw)
-    except Exception, exp:
-        common.email_error(exp, raw)
+def shutdown():
+    ''' Stop this app '''
+    log.msg("Shutting down...")
+    reactor.callWhenRunning(reactor.stop)
 
 
-def real_process(raw):
+class MyProductIngestor(ldmbridge.LDMProductReceiver):
+    """ I receive products from ldmbridge and process them 1 by 1 :) """
+
+    def connectionLost(self, reason):
+        ''' callback when the stdin reader connection is closed '''
+        log.msg('connectionLost() called...')
+        log.err(reason)
+        reactor.callLater(7, shutdown)
+
+    def process_data(self, buf):
+        """ Process the product """
+        df = POSTGIS.runInteraction(real_process, buf)
+        df.addErrback(common.email_error, buf)
+        df.addErrback(log.err)
+
+
+def real_process(txn, raw):
     sqlraw = raw.replace("\015\015\012", "\n")
     prod = product.TextProduct(raw)
 
     product_id = prod.get_product_id()
-    sql = """INSERT into text_products(product, product_id) values (%s,%s)"""
-    myargs = (sqlraw, product_id)
-    POSTGIS.runOperation(sql, myargs)
+    if DB_ON:
+        sql = """
+            INSERT into text_products(product, product_id) values (%s,%s)
+        """
+        myargs = (sqlraw, product_id)
+        txn.execute(sql, myargs)
 
     tokens = re.findall("ATTN (WFOS|RFCS)(.*)", raw)
+    channels = []
     for tpair in tokens:
-        wfos = re.findall("([A-Z]+)\.\.\.", tpair[1])
-        # Remember, xtra is passed below by reference, so we need to reinit it
-        xtra = {'product_id': product_id}
-        xtra['channels'] = ','.join(wfos)
-        xtra['twitter'] = ("NESDIS issues Satellite Precipitation "
-                           "Estimates %s?pid=%s") % (PYWWA_PRODUCT_URL,
-                                                     product_id)
+        for center in re.findall("([A-Z]+)\.\.\.", tpair[1]):
+            channels.append("SPENES.%s" % (center, ))
+    xtra = {'product_id': product_id}
+    xtra['channels'] = ','.join(channels)
+    xtra['twitter'] = ("NESDIS issues Satellite Precipitation "
+                       "Estimates %s?pid=%s") % (PYWWA_PRODUCT_URL,
+                                                 product_id)
 
-        body = ("NESDIS issues Satellite Precipitation Estimates %s?pid=%s"
+    body = ("NESDIS issues Satellite Precipitation Estimates %s?pid=%s"
+            ) % (PYWWA_PRODUCT_URL, product_id)
+    htmlbody = ("<p>NESDIS issues "
+                "<a href='%s?pid=%s'>Satellite Precipitation Estimates</a>"
+                "</p>"
                 ) % (PYWWA_PRODUCT_URL, product_id)
-        htmlbody = ("<p>NESDIS issues "
-                    "<a href='%s?pid=%s'>Satellite Precipitation Estimates</a>"
-                    "</p>"
-                    ) % (PYWWA_PRODUCT_URL, product_id)
-        jabber.sendMessage(body, htmlbody, xtra)
+    jabber.sendMessage(body, htmlbody, xtra)
 
 
 def killer():
@@ -59,6 +78,5 @@ def killer():
 
 if __name__ == '__main__':
     jabber = common.make_jabber_client("spe_parser")
-    reactor.callLater(0, process, raw)
-    reactor.callLater(30, killer)
+    ldmbridge.LDMProductFactory(MyProductIngestor(dedup=True))
     reactor.run()
