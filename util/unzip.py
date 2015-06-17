207af56cac730210d61be52412048d5adbfc7a2c
akrherz <akrherz@iastate.edu>
2015-06-17 15:10:25 -0500

    make the unzip process more atomic

--- util/unzip.py
+++ util/unzip.py
@@ -5,31 +5,50 @@ called from pqact_iemvs.conf
 """
 import sys
 import os
-import subprocess
+import zipfile
+import StringIO
+import random
+import string
 
 BASE = "/home/ldm/data"
 
+
+def ranstr():
+    return ''.join(random.SystemRandom(
+                    ).choice(string.ascii_uppercase + string.digits)
+                   for _ in range(8))
+
+
 def main():
-    """ Do something important """
-    data = sys.stdin.read()
+    """ Read the zipfile from stdin and do important things """
+    data = StringIO.StringIO(sys.stdin.read())
+    zf = zipfile.ZipFile(data)
     filename = sys.argv[1]
     # Makedir if it does not exist
     dirname = "%s/%s" % (BASE, os.path.dirname(filename))
     if not os.path.isdir(dirname):
         os.makedirs(dirname)
 
+    work = []
+    for info in zf.infolist():
+        fn = info.filename
+        tmpfn = "%s/%s_%s" % (dirname, ranstr(), fn)
+        newfn = "%s/%s" % (dirname, fn)
+        output = open(tmpfn, 'wb')
+        output.write(zf.read(fn))
+        output.close()
+        work.append([tmpfn, newfn])
+
+    for [tmpfn, fn] in work:
+        # print '%s -> %s' % (tmpfn, fn)
+        os.rename(tmpfn, fn)
+
     # Write the file
+    data.seek(0)
     output = open("%s/%s" % (BASE, filename,), 'wb')
-    output.write(data)
+    output.write(data.read())
     output.close()
 
-    proc = subprocess.Popen("unzip -d %s -o %s/%s" % (dirname, BASE, filename),
-                            shell=True, stderr=subprocess.PIPE,
-                            stdout=subprocess.PIPE)
-    res = proc.stderr.read()
-    if res != "":
-        print res
-        sys.exit(1)
 
 if __name__ == '__main__':
     main()
