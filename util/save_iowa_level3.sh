cfb35b57e17abf72da1f1e19ced9ba669c874c71
akrherz <akrherz@iastate.edu>
2015-12-04 10:35:41 -0600

    use CyBox to store these files

--- util/save_iowa_level3.sh
+++ util/save_iowa_level3.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-# Simple Script that will save RADAR data
+# Save Iowa Level III data to cybox 
 # Run from cron
 #set -x
 
 DIR=$(date -u -d "1 day ago" +'%Y_%m')
 YYYYMMDD=$(date -u -d "1 day ago" +'%Y%m%d')
-
-if [ ! -d /mesonet/ARCHIVE/nexrad/${DIR} ] 
-then
-	mkdir -p /mesonet/ARCHIVE/nexrad/${DIR}
-fi
+YYYY=$(date -u -d "1 day ago" +'%Y')
+MM=$(date -u -d "1 day ago" +'%m')
 
 for R in $(echo "DMX DVN OAX ARX FSD MPX EAX ABR UDX"); do
 cd /mnt/nexrad3/nexrad/NIDS/${R}
@@ -17,8 +14,17 @@ cd /mnt/nexrad3/nexrad/NIDS/${R}
   tar -uf /tmp/${R}radar.tar ?[0-9]?/???_${YYYYMMDD}_*
   cd /tmp
   gzip -c ${R}radar.tar > ${R}radar.tgz
-  mv ${R}radar.tgz /mesonet/ARCHIVE/nexrad/${DIR}/${R}_${YYYYMMDD}.tgz
-  rm ${R}radar.tar
+mv ${R}radar.tgz ${R}_${YYYYMMDD}.tgz
+lftp -u akrherz@iastate.edu ftps://ftp.box.com << EOM
+cd IowaNexrad3
+mkdir ${YYYY}
+cd ${YYYY}
+mkdir ${MM}
+cd ${MM}
+put ${R}radar.tgz
+bye
+EOM
+rm ${R}radar.tar ${R}_${YYYYMMDD}.tgz
 done
 
 #END
