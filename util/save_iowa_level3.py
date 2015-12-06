a53a92b3ee91cf3f7129e28b492b053e08707110
akrherz <akrherz@iastate.edu>
2015-12-06 15:21:13 -0600

    migrate L3 script to python based for more control

---
+++ util/save_iowa_level3.py
@@ -0,0 +1,31 @@
+"""Save our local NEXRAD level 3 RADARs to CyBox"""
+import datetime
+import os
+import subprocess
+from pyiem.util import send2box
+
+NEXRADS = "DMX DVN OAX ARX FSD MPX EAX ABR UDX".split()
+
+
+def run(date):
+    for nexrad in NEXRADS:
+        os.chdir("/mnt/nexrad3/nexrad/NIDS/%s" % (nexrad, ))
+        cmd = ("tar -czf /tmp/%sradar.tgz ???/???_%s_*"
+               ) % (nexrad, date.strftime("%Y%m%d"))
+        subprocess.call(cmd, shell=True)
+
+    filenames = ['/tmp/%sradar.tgz' % (x, ) for x in NEXRADS]
+    remotenames = ['%s_%s.tgz' % (x, date.strftime("%Y%m%d")) for x in NEXRADS]
+    send2box(filenames, "/IowaNexrad3/%s" % (date.strftime("%Y/%m"),),
+             remotenames=remotenames)
+    for filename in filenames:
+        if os.path.isfile(filename):
+            os.unlink(filename)
+
+
+def main():
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    run(yesterday)
+
+if __name__ == '__main__':
+    main()
