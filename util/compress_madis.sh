6d91a68a5e7d3af49d766048b1f19a85d8665bc9
akrherz <akrherz@iastate.edu>
2016-05-02 08:09:22 -0500

    update to use new MADIS paths

--- util/compress_madis.sh
+++ util/compress_madis.sh
@@ -4,15 +4,13 @@
 YYYYMMDD=$(date --date '1 day ago' +'%Y%m%d')
 YYYY=$(date --date '1 day ago' +'%Y')
 
-cd /mesonet/data/madis/metar
+dirs=("metar" "mesonet1" "hfmetar")
+for p in "${dirs[@]}"
+do
+cd /mesonet/data/madis/$p
 gzip ${YYYYMMDD}*nc
-mkdir -p /mnt/longterm2/madis/metar/${YYYY}
-mv ${YYYYMMDD}*nc.gz /mnt/longterm2/madis/metar/${YYYY}
-
-cd /mesonet/data/madis/mesonet
-gzip ${YYYYMMDD}*nc
-mkdir -p /mnt/longterm2/madis/mesonet/${YYYY}
-mv ${YYYYMMDD}*nc.gz /mnt/longterm2/madis/mesonet/${YYYY}
+mkdir -p /mnt/longterm2/madis/$p/${YYYY}
+mv ${YYYYMMDD}*nc.gz /mnt/longterm2/madis/$p/${YYYY}
+done
 
 #END
-
