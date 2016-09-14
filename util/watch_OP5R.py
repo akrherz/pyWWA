8f093794934643ab13195cbc44d08bfa97d37e89
akrherz <akrherz@iastate.edu>
2016-09-14 14:29:46 -0500

    OP5R watch is now dumping to LDM

--- util/watch_OP5R.py
+++ util/watch_OP5R.py
@@ -2,7 +2,6 @@
 import inotify.adapters
 import re
 import os
-import shutil
 import subprocess
 
 FNPATTERN = re.compile("OP5R_[0-9]{8}_[0-9]{4}")
@@ -11,7 +10,7 @@ FNPATTERN = re.compile("OP5R_[0-9]{8}_[0-9]{4}")
 def main():
     i = inotify.adapters.Inotify()
 
-    i.add_watch('/local/op5r')
+    i.add_watch('/home/op5r')
     try:
         for event in i.event_gen():
             if event is not None:
@@ -22,13 +21,10 @@ def main():
                 if not FNPATTERN.match(fn):
                     print("invalid filename: %s" % (fn,))
                     continue
-                shutil.move("%s/%s" % (watch_path, fn),
-                            "/mnt/level2/raw/OP5R/%s" % (fn,))
-                os.chdir("/mnt/level2/raw/OP5R")
-                subprocess.call(("ls -l OP5R* | "
-                                 "awk '{print $5 \" \" $9}' > dir2.list"
-                                 ), shell=True)
-                shutil.move("dir2.list", "dir.list")
+                cmd = ("/home/ldm/bin/pqinsert -f NEXRAD2 -p '%s' %s/%s"
+                       ) % (fn, watch_path, fn)
+                subprocess.call(cmd, shell=True)
+                os.unlink("%s/%s" % (watch_path, fn))
 
     finally:
         i.remove_watch('/local/op5r')
