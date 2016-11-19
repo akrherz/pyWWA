4f7621cf4d73129d404141d75c9c6e14b6c43a0e
akrherz <akrherz@iastate.edu>
2016-11-18 21:40:53 -0600

    correct web URI

--- util/save_rc.sh
+++ util/save_rc.sh
@@ -5,7 +5,7 @@
 ts="$1"
 hhmm=$(echo $1 | cut -c 9-12)
 
-wget -q -O /tmp/iaroads.png 'http://iemvs101.local/roads/iem.php?8bit'
+wget -q -O /tmp/iaroads.png 'http://iem.local/roads/iem.php?8bit'
 
 pqinsert -p "plot ac $ts iaroads.png iaroads_${hhmm}.png png" /tmp/iaroads.png
 
