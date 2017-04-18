f922f5b0c11bcaa0a4a136090ba0f1334d76855d
akrherz <akrherz@iastate.edu>
2017-04-18 10:57:30 -0500

    remove unused current_ww.zip entry and action script

--- util/save_ww.sh
+++
@@ -1,8 +0,0 @@
-#
-# Save the zip file of current NWS WWA
-# TODO: this should be removed in favour of unzip.py
-#
-
-cd /mesonet/data/gis/shape/4326/us
-cat > current_ww.zip
-unzip -o current_ww.zip >& /dev/null
