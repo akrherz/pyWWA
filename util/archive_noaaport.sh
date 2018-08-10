599437b047773c70b14bb472b7dff7a5efb9526b
akrherz <akrherz@iastate.edu>
2018-08-10 10:00:39 -0500

    cleanup noaaport text archival from wxstation removal

--- util/archive_noaaport.sh
+++ util/archive_noaaport.sh
@@ -5,10 +5,24 @@
 dd=$(date --date "$1 day ago" +'%d')
 yyyymmdd=$(date --date "$1 day ago" +'%Y%m%d')
 yyyy=$(date --date "$1 day ago" +'%Y')
+mm=$(date --date "$1 day ago" +'%m')
 
 cd ~/offline/text/
 tar -czf ${yyyymmdd}.tgz ${yyyymmdd}??.txt
 rm -f ${yyyymmdd}??.txt
 mkdir -p /mesonet/ARCHIVE/raw/noaaport/$yyyy
+
+# Upload this file to box
+lftp -u akrherz@iastate.edu ftps://ftp.box.com << EOM
+cd NOAAPortText
+mkdir ${yyyy}
+cd ${yyyy}
+mkdir ${mm}
+cd ${mm}
+put ${yyyymmdd}.tgz
+bye
+EOM
+
+
 mv ${yyyymmdd}.tgz /mesonet/ARCHIVE/raw/noaaport/$yyyy/
 # END
