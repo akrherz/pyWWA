18ff1567ce6e1639f31b6343a470455f953dfd26
akrherz <akrherz@iastate.edu>
2020-03-19 04:43:23 -0500

    migrate to stage / gdrive uploads

--- util/save_iowa_level3.py
+++ util/save_iowa_level3.py
@@ -1,36 +1,38 @@
-"""Save our local NEXRAD level 3 RADARs to CyBox."""
+"""Save our local NEXRAD level 3 RADARs to staging."""
 import datetime
 import os
 import subprocess
 
-# import logging
-from pyiem.ftpsession import send2box
+from pyiem.util import logger
 
+LOG = logger()
 NEXRADS = "DMX DVN OAX ARX FSD MPX EAX ABR UDX".split()
-# log = logging.getLogger()
-# log.setLevel(logging.DEBUG)
 
 
 def run(date):
     """Process this date please"""
+    yyyymmdd = date.strftime("%Y%m%d")
     for nexrad in NEXRADS:
-        os.chdir("/mnt/nexrad3/nexrad/NIDS/%s" % (nexrad,))
-        cmd = ("tar -czf /mesonet/tmp/%sradar.tgz ???/???_%s_*") % (
+        mydir = "/mnt/nexrad3/nexrad/NIDS/%s" % (nexrad,)
+        if not os.path.isdir(mydir):
+            LOG.info("creating %s", mydir)
+            os.makedirs(mydir)
+        os.chdir(mydir)
+        cmd = ("tar -czf /mesonet/tmp/%s_%s.tgz ???/???_%s_*") % (
             nexrad,
-            date.strftime("%Y%m%d"),
+            yyyymmdd,
+            yyyymmdd,
         )
+        LOG.debug(cmd)
         subprocess.call(cmd, shell=True)
 
-    filenames = ["/mesonet/tmp/%sradar.tgz" % (x,) for x in NEXRADS]
-    remotenames = ["%s_%s.tgz" % (x, date.strftime("%Y%m%d")) for x in NEXRADS]
-    send2box(
-        filenames,
-        "/IowaNexrad3/%s" % (date.strftime("%Y/%m"),),
-        remotenames=remotenames,
-    )
-    for filename in filenames:
-        if os.path.isfile(filename):
-            os.unlink(filename)
+    rpath = "/stage/IowaNexrad3/%s" % (date.strftime("%Y/%m"),)
+    cmd = (
+        'rsync --remove-source-files -a --rsync-path "mkdir -p %s && rsync" '
+        "/mesonet/tmp/???_%s.tgz meteor_ldm@metl60.agron.iastate.edu:%s"
+    ) % (rpath, yyyymmdd, rpath)
+    LOG.debug(cmd)
+    subprocess.call(cmd, shell=True)
 
 
 def main():
