4fcc603bcfea53b99993e6f2f76ab273b006d8b9
akrherz <akrherz@iastate.edu>
2020-11-25 07:10:58 -0600

    add test coverage

---
+++ setup.py
@@ -0,0 +1,4 @@
+"""our setup file."""
+from setuptools import setup
+
+setup(use_scm_version={"version_scheme": "post-release"})
---
+++ tests/test_common.py
@@ -0,0 +1,8 @@
+"""Test pywwa.common"""
+
+from pywwa import common
+
+
+def test_crawl():
+    """Test crawling before walking."""
+    assert common
