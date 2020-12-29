e5008989dfbcbecbea9e7a3aa9f136b8b26f3b19
akrherz <akrherz@iastate.edu>
2020-12-29 14:11:12 -0600

    migrate cli_parser business logic to pyIEM

---
+++ tests/workflows/test_cli_parser.py
@@ -0,0 +1,40 @@
+"""Test cli_parser."""
+# 3rd Party
+from pyiem.util import utc
+import pytest
+
+# Local
+from pywwa import common
+from pywwa.workflows import cli_parser
+from pywwa.testing import get_example_file
+
+
+@pytest.mark.parametrize("database", ["iem"])
+def test_processor(cursor):
+    """Test basic parsing."""
+    data = get_example_file("CLI.txt")
+    common.CTX.utcnow = utc(2015, 6, 9, 6, 51)
+    prod = cli_parser.processor(cursor, data)
+    assert prod.valid == common.CTX.utcnow
+
+
+@pytest.mark.parametrize("database", ["iem"])
+def test_two_clis(cursor):
+    """Test parsing ye infamous double CLI."""
+    cursor.execute(
+        "select max_tmpf from summary_2014 s JOIN stations t "
+        "on (s.iemid = t.iemid) WHERE t.id in ('HOU', 'IAH') "
+        "and day = '2014-11-30'"
+    )
+    data = get_example_file("CLIHOU.txt")
+    prod = cli_parser.processor(cursor, data)
+    assert len(prod.data) == 2
+
+
+@pytest.mark.parametrize("database", ["iem"])
+def test_bad_station(cursor):
+    """Test what happens when we have an unknown station."""
+    data = get_example_file("CLI.txt").replace("CLIFGF", "CLIXXX")
+    common.CTX.utcnow = utc(2015, 6, 9, 6, 51)
+    prod = cli_parser.processor(cursor, data)
+    assert prod is not None
