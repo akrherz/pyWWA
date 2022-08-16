655490344046b9fc24e6f433643e52340b9371af
akrherz <akrherz@iastate.edu>
2022-08-16 10:31:55 -0500

    add archiver of NWWSOI

---
+++ util/archive_nwwsoi.sh
@@ -0,0 +1,13 @@
+# Archive NWWS-OI data from 7 days ago
+
+yyyymmdd=$(date --date "$7 day ago" +'%Y%m%d')
+yyyy=$(date --date "$7 day ago" +'%Y')
+
+cd /mesonet/tmp/nwwsoi/
+tar -czf ${yyyymmdd}.tgz ${yyyymmdd}??.txt
+rm -f ${yyyymmdd}??.txt
+
+rpath="/stage/NWWSOI/${yyyy}/${mm}"
+rsync -a --remove-source-files --rsync-path "mkdir -p $rpath && rsync" ${yyyymmdd}.tgz meteor_ldm@metl60.agron.iastate.edu:$rpath
+
+# END
