7f3530106b60a58643f5f95a2f7b15d5fb2b7caa
akrherz <akrherz@iastate.edu>
2023-01-25 12:11:37 -0600

    rework MADIS storage due to processing troubles

---
+++ util/compress_madis.py
@@ -0,0 +1,60 @@
+"""Archive MADIS files."""
+from datetime import date, timedelta
+import glob
+import os
+import subprocess
+
+from pyiem.util import logger
+
+LOG = logger()
+HOST = "metl60.agron.iastate.edu"
+
+
+def process(mydir, valid):
+    """Do the necessary work."""
+    os.chdir(f"/mesonet/data/madis/{mydir}")
+    # Collapse files down.
+    for hr in range(0, 24):
+        i = 300
+        found = False
+        while i >= 0:
+            fn = f"{valid:%Y%m%d}_{hr:02.0f}00_{i}.nc"
+            if os.path.isfile(fn):
+                if not found:
+                    found = True
+                    cmd = ["cp", "-f", fn, f"{valid:%Y%m%d}_{hr:02.0f}00.nc"]
+                    subprocess.call(cmd)
+                os.unlink(fn)
+            i -= 1
+    # gzip
+    files = glob.glob(f"{valid:%Y%m%d}*nc")
+    if not files:
+        LOG.info("No files found for %s", mydir)
+        return
+    subprocess.call(["gzip", *files])
+    files = glob.glob(f"{valid:%Y%m%d}*nc.gz")
+    if not files:
+        LOG.info("No gzip files found for %s", mydir)
+        return
+    # rsync
+    cmd = [
+        "rsync",
+        "-a",
+        "--remove-source-files",
+        "--rsync-path",
+        f"mkdir -p /stage/iemoffline/madis/{mydir}/{valid:%Y} && rsync",
+        *files,
+        f"{HOST}:/stage/iemoffline/madis/{mydir}/{valid:%Y}/",
+    ]
+    subprocess.call(cmd)
+
+
+def main():
+    """Go Main Go."""
+    valid = date.today() - timedelta(days=1)
+    for mydir in ["mesonet", "hfmetar", "mesonet1", "metar"]:
+        process(mydir, valid)
+
+
+if __name__ == "__main__":
+    main()
--- util/store_madis.sh
+++ util/store_madis.sh
@@ -1,9 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 # Store the MADIS netCDF files to where they should go $1
 
 gzip -d > /tmp/$$.$2
 F=$(echo $2 | cut -c1-13)
-# Had an issue with random failures on some days, so remove files if fail
-mv /tmp/$$.$2 $1/$F.nc || rm -f /tmp/$$.$2
+
+# We have some time intensive readers, so we keep copies around
+for i in $(seq 0 300); do
+    if [ -f $1/${F}_${i}.nc ]; then
+        continue;
+    fi
+    # Had an issue with random failures on some days, so remove files if fail
+    mv /tmp/$$.$2 $1/${F}_${i}.nc || rm -f /tmp/$$.$2
+    break;
+done
+
 
 # End
