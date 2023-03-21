eb91ee947b4348a37ae57457ef5591d1c8a84bed
akrherz <akrherz@iastate.edu>
2023-03-20 23:05:35 -0500

    add bling to damage survey PNS email

--- tests/workflows/test_spammer.py
+++ tests/workflows/test_spammer.py
@@ -1,8 +1,24 @@
 """Test spammer."""
 
+from pyiem.nws.product import TextProduct
+
 # Local
 from pywwa.workflows import spammer
 from pywwa.testing import get_example_file
+
+
+def test_tornado_multi():
+    """Test the parsing of EF scale."""
+    prod = TextProduct(get_example_file("PNS_damage_multi.txt"))
+    res = spammer.damage_survey_pns(prod)
+    assert "EF1" in res[0]
+
+
+def test_tornado_damage():
+    """Test the parsing of EF scale."""
+    prod = TextProduct(get_example_file("PNS_damage.txt"))
+    res = spammer.damage_survey_pns(prod)
+    assert "EF2" in res[0]
 
 
 def test_spammer():
