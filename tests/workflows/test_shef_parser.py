b198d5d70802f9ce32605fa83b9d508fb67b75c3
akrherz <akrherz@iastate.edu>
2023-05-15 13:39:38 -0500

    save SHEF in more cases to iemaccess

--- tests/workflows/test_shef_parser.py
+++ tests/workflows/test_shef_parser.py
@@ -13,6 +13,56 @@ from pywwa.workflows import shef_parser
 from twisted.python.failure import Failure
 
 
+def sync_workflow(prod, cursor):
+    """Common workflow."""
+    mydata = shef_parser.restructure_data(prod)
+    print(mydata)
+    for sid, data in mydata.items():
+        times = list(data.keys())
+        times.sort()
+        for tstamp in times:
+            print(tstamp)
+            shef_parser.process_site_time(
+                cursor, prod, sid, tstamp, data[tstamp])
+
+
+@pytest.mark.parametrize("database", ["iem"])
+def test_omit_report(cursor):
+    """Test that the report is omitted..."""
+    pywwa.CTX.utcnow = utc(2023, 5, 12, 18)
+    cursor.execute(
+        "INSERT into stations(id, network, tzname, iemid) VALUES "
+        "('CORM6', 'MS_DCP', 'America/Chicago', -99)"
+    )
+
+    shef_parser.LOCS["CORM6"] = {"MS_COOP": {
+        "valid": shef_parser.U1980,
+        "iemid": -99,
+        "tzname": "America/Chicago",
+        "epoc": 1,
+        "pedts": 1,
+    }}
+    prod = shef_parser.process_data(get_example_file("CORM6.txt"))
+    sync_workflow(prod, cursor)
+    cursor.execute(
+        "SELECT report, max_tmpf from summary_2023 "
+        "where iemid = -99 and day = '2023-05-12'"
+    )
+    row = cursor.fetchone()
+    assert row[1] == 83
+    assert row[0] is not None
+    ans = row[0]
+    prod = shef_parser.process_data(get_example_file("CORM6_RTP.txt"))
+    sync_workflow(prod, cursor)
+    cursor.execute(
+        "SELECT report, max_tmpf from summary_2023 "
+        "where iemid = -99 and day = '2023-05-12'"
+    )
+    row = cursor.fetchone()
+    assert row[1] == 84
+    assert row[0] == ans
+
+
 def test_process_site_eb():
     """Test that the errorback works without any side effects."""
     prod = shef_parser.process_data(get_example_file("RR7.txt"))
