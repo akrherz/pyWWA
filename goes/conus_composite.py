74c1ef20231bcf5b0d8bb9a2f54227be3de4d7ab
akrherz <akrherz@iastate.edu>
2023-06-13 17:31:59 -0500

    cleanup and fix goes composite script

--- goes/conus_composite.py
+++ goes/conus_composite.py
@@ -4,14 +4,17 @@
    appears CONUS goes west goes to just -112 east?
 2. gdal_merge.py: combine these two with the west file last (on top)
 """
+import datetime
 import json
 import os
 import subprocess
 import sys
 import tempfile
+from zoneinfo import ZoneInfo
 
 from pyiem.util import logger, utc
 
+ISO = "%Y-%m-%dT%H:%M:%SZ"
 LOG = logger()
 PATH = "/mesonet/ldmdata/gis/images/GOES/conus"
 LOOKUP = {2: "vis", 9: "wv", 13: "ir"}
@@ -33,44 +36,54 @@ def call(cmd):
     stderr = proc.stderr.read()
     if stdout != b"":
         if not filtermsg(stdout):
-            LOG.info(cmd)
-            LOG.info(stdout)
+            LOG.warning(cmd)
+            LOG.warning(stdout)
     if stderr != b"":
         if not filtermsg(stderr):
-            LOG.info(cmd)
-            LOG.info(stderr)
+            LOG.warning(cmd)
+            LOG.warning(stderr)
 
 
 def run(valid, channel, tmpname):
     """Do work."""
-    for bird in [16, 18]:
+    tiles = []
+    for bird in [16, 17, 18]:
         fnbase = "%s/channel%02i/GOES-%s_C%02i" % (
             PATH,
             channel,
             bird,
             channel,
         )
-        info = json.load(open(fnbase + ".json", encoding="utf8"))
+        with open(f"{fnbase}.json", encoding="utf8") as fp:
+            info = json.load(fp)
+        ts = datetime.datetime.strptime(info["meta"]["valid"], ISO).replace(
+            tzinfo=ZoneInfo("UTC")
+        )
+        if (valid - ts) > datetime.timedelta(hours=6):
+            continue
         # step 1
+        fn = f"{tmpname}_{bird}.tif"
+        tiles.append(fn)
         cmd = (
             "gdalwarp -q -s_srs '%s' -t_srs 'EPSG:4326' -te %s %s %s %s "
-            "-tr 0.04 0.04 -of GTiff %s.png %s_%s.tif"
+            f"-tr 0.04 0.04 -of GTiff %s.png {fn}"
         ) % (
             info["meta"]["proj4str"],
-            -140 if bird == 17 else -113.1,
+            -140 if bird in [17, 18] else -113.1,
             22,
-            -112.9 if bird == 17 else -60,
+            -112.9 if bird in [17, 18] else -60,
             52,
             fnbase,
-            tmpname,
-            bird,
         )
         call(cmd)
 
+    if not tiles:
+        LOG.warning("No data found for tiling...")
+        return
     cmd = (
         "gdal_merge.py -q -o %s.tif -of GTiff -ps 0.04 0.04 "
-        "-ul_lr -130 52 -60 22 %s_17.tif %s_16.tif"
-    ) % (tmpname, tmpname, tmpname)
+        "-ul_lr -130 52 -60 22 %s"
+    ) % (tmpname, " ".join(tiles))
     call(cmd)
 
     # GIS/sat/conus_goes_vis4km_1045.tif
@@ -90,19 +103,18 @@ def run(valid, channel, tmpname):
 def main(argv):
     """Go Main Go."""
     valid = utc(*[int(a) for a in argv[1:6]])
-    LOG.debug("valid is set to: %s", valid)
+    LOG.info("valid is set to: %s", valid)
     for channel in [2, 9, 13]:
         with tempfile.NamedTemporaryFile() as tmpfd:
             try:
                 run(valid, channel, tmpfd.name)
             except Exception as exp:
                 LOG.exception(exp)
-            finally:
-                for part in ["", "_16", "_17"]:
-                    fn = f"{tmpfd.name}{part}.tif"
-                    if not os.path.isfile(fn):
-                        continue
-                    os.unlink(fn)
+        for part in ["", "_16", "_17", "_18"]:
+            fn = f"{tmpfd.name}{part}.tif"
+            if not os.path.isfile(fn):
+                continue
+            os.unlink(fn)
 
 
 if __name__ == "__main__":
