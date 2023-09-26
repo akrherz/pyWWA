97b4a82f25868b00036ba78022dcf997485f7326
akrherz <akrherz@iastate.edu>
2023-09-26 18:45:22 -0500

    add util feeder script

---
+++ util/prod_feeder_loop.py
@@ -0,0 +1,16 @@
+"""Some magic to feed the SEL, SAW, and WWP products into the LDM bridge."""
+import sys
+import time
+
+
+def main(argv):
+    """Go Main Go."""
+    with open(argv[1], "rb") as fh:
+        payload = fh.read()
+    while True:
+        sys.stdout.buffer.write(payload)
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
