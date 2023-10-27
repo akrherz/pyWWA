1208d72bacba71475f5160f1f754c11ff7dbe588
akrherz <akrherz@iastate.edu>
2023-10-26 22:39:45 -0500

    fix: refactor IEMAccess logic for #181

--- tests/workflows/test_shef_parser.py
+++ tests/workflows/test_shef_parser.py
@@ -1,5 +1,7 @@
 """Test shef_parser."""
+import datetime
 from functools import partial
+from zoneinfo import ZoneInfo
 
 # 3rd Party
 # pylint: disable=no-member-in-module
@@ -26,9 +28,57 @@ def run_interaction(cursor, sql, args):
 def sync_workflow(prod, cursor):
     """Common workflow."""
     mydata = shef_parser.restructure_data(prod)
-    print(mydata)
     for sid, data in mydata.items():
-        shef_parser.process_site(cursor, prod, sid, data)
+        shef_parser.process_site(prod, sid, data)
+    # Just exercise the API
+    shef_parser.process_accessdb_frontend()
+
+    # Arm the entries for processing
+    ts = utc() - datetime.timedelta(days=1)
+    for iemid, entry in shef_parser.ACCESSDB_QUEUE.items():
+        for localts, record in entry.records.items():
+            record["last"] = ts
+            shef_parser.write_access_records(
+                cursor, [(localts, record)], iemid, entry
+            )
+    # Just exercise the API
+    shef_parser.process_accessdb_frontend()
+
+
+def test_get_localtime():
+    """Test when we don't know of a station."""
+    tzinfo = ZoneInfo("America/Chicago")
+    assert shef_parser.get_localtime("XX_DCP123456", tzinfo) == tzinfo
+
+
+def test_231027_rr8msr_novariable():
+    """Test this found in the wild."""
+    pywwa.CTX.utcnow = utc(2023, 10, 19, 15)
+    prod = shef_parser.process_data(get_example_file("SHEF/RR8MSR.txt"))
+    assert not prod.data
+
+
+def test_get_network():
+    """Test get_network logic."""
+    prod = mock.Mock()
+    prod.afos = "RR1KRF"
+    elem = shef_parser.SHEFElement(
+        station="AZZI4",
+        basevalid=utc(),
+        valid=utc(),
+        physical_element="XX",
+        duration="I",
+        extremum="Z",
+        value=0,
+    )
+    assert (
+        shef_parser.get_network(
+            prod,
+            "AZZI4",
+            [elem],
+        )
+        == "IA_DCP"
+    )
 
 
 def test_empty_product():
@@ -60,6 +110,13 @@ def test_230926_rr8krf(cursor):
     prod = shef_parser.process_data(get_example_file("SHEF/RR8KRF.txt"))
     for element in prod.data:
         shef_parser.insert_raw_inbound(cursor, element)
+
+
+def test_bad_element_in_current_queue():
+    """Test GIGO on current_queue."""
+    shef_parser.CURRENT_QUEUE.clear()
+    shef_parser.CURRENT_QUEUE["HI|BYE|HI"] = {"dirty": True}
+    shef_parser.save_current()
 
 
 @pytest.mark.parametrize("database", ["iem"])
@@ -103,26 +160,31 @@ def test_omit_report(cursor):
 
     shef_parser.ACCESSDB.runOperation = partial(run_interaction, cursor)
     assert shef_parser.save_current() == 7
+    assert shef_parser.save_current() == 0
 
 
-@pytest.mark.parametrize("database", ["iem"])
-def test_process_site_eb(cursor):
+def test_process_site_eb():
     """Test that the errorback works without any side effects."""
     pywwa.CTX.utcnow = utc(2017, 8, 15, 14)
-    prod = shef_parser.process_data(get_example_file("RR7.txt"))
-    sync_workflow(prod, cursor)
-    shef_parser.process_site_eb(Failure(Exception("Hi Daryl")), prod, "", {})
-    shef_parser.process_site_eb(Failure(DeadlockDetected()), prod, "", {})
+    shef_parser.process_data(get_example_file("RR7.txt"))
+    entry = shef_parser.ACCESSDB_ENTRY(
+        station="", network="", tzname="", records={}
+    )
+    record = {"data": {}, "last": utc(), "product_id": ""}
+    shef_parser.write_access_records_eb(
+        Failure(Exception("Hi Daryl")), [(utc(), record)], 0, entry
+    )
+    shef_parser.write_access_records_eb(
+        Failure(DeadlockDetected()), [(utc, record)], 0, entry
+    )
 
 
-@pytest.mark.parametrize("database", ["iem"])
-def test_checkvars(cursor):
+def test_checkvars():
     """Excerise the checkvars logic with the RR1.txt example"""
     payload = get_example_file("SHEF/RR1.txt")
     pywwa.CTX.utcnow = utc(2022, 11, 10, 12)
     for repl in ["TX", "HG", "SF", "PPH"]:
-        prod = shef_parser.process_data(payload.replace("/TX", f"/{repl}"))
-        sync_workflow(prod, cursor)
+        shef_parser.process_data(payload.replace("/TX", f"/{repl}"))
 
 
 def test_restructure_data_eightchar_id():
