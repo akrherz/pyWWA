002d6b701089e06eb986b2f59d8407b79d852af6
akrherz <akrherz@iastate.edu>
2024-01-09 15:13:28 -0600

    fix: simplify and test mos workflow

--- src/pywwa/workflows/mos.py
+++ src/pywwa/workflows/mos.py
@@ -1,6 +1,4 @@
 """ MOS Data Ingestor, why not? """
-from functools import partial
-
 # 3rd Party
 import click
 from pyiem.nws.products.mos import parser
@@ -10,34 +8,15 @@ from pywwa import common
 from pywwa.database import get_database
 from pywwa.ldm import bridge
 
-MEMORY = {"ingested": 0}
 
-
-def process_data(data):
-    """
-    Actual ingestor
-    """
-    try:
-        real_process(data)
-    except Exception as myexp:
-        common.email_error(myexp, data)
-
-
-def got_data(res):
-    """Callback from the database save"""
-    MEMORY["ingested"] += res
-
-
-def real_process(dbpool, text):
+def process_data(txn, text):
     """The real processor of the raw data, fun!"""
     prod = parser(text, utcnow=common.utcnow())
     if not common.dbwrite_enabled():
         return
     if prod.warnings:
         common.email_error("\n".join(prod.warnings), text)
-    df = dbpool.runInteraction(prod.sql)
-    df.addCallback(got_data)
-    df.addErrback(common.email_error, text)
+    prod.sql(txn)
 
 
 @click.command(help=__doc__)
@@ -45,5 +24,4 @@ def real_process(dbpool, text):
 @common.disable_xmpp
 def main(*args, **kwargs):
     """Go Main Go."""
-    func = partial(process_data, get_database("mos", cp_max=5))
-    bridge(func)
+    bridge(process_data, dbpool=get_database("mos", cp_max=5))
---
+++ tests/workflows/test_mos.py
@@ -0,0 +1,11 @@
+"""test mos workflow"""
+
+import pytest
+from pywwa.testing import get_example_file
+from pywwa.workflows.mos import process_data
+
+
+@pytest.mark.parametrize("database", ["mos"])
+def test_workflow(cursor):
+    """Test."""
+    process_data(cursor, get_example_file("METNC1.txt"))
