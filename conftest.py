eb18a88fc85bf1832d23b0acc6b9a08aae446d3c
akrherz <akrherz@iastate.edu>
2024-01-10 21:18:15 -0600

    fix tests

--- conftest.py
+++ conftest.py
@@ -1,21 +1,21 @@
 """For testing."""
 # 3rd party
 import pytest
-import pywwa
+from pywwa import CTX, CTX_DEFAULTS
 from pywwa.database import get_dbconnc
 
 # Dragons
 # pytest + twisted + click == too much magic for poor me
 # So, this effectively disables reactor.stop() from working
-pywwa.CTX_DEFAULTS["shutdown_delay"] = 1800
+CTX_DEFAULTS["shutdown_delay"] = 1800
 
 
 @pytest.fixture(autouse=True)
 def setup_and_teardown():
     """Something to fix the hacky global namespace mucking I do."""
-    pywwa.CTX.update(pywwa.CTX_DEFAULTS)
+    CTX.update(CTX_DEFAULTS)
     yield
-    pywwa.CTX.update(pywwa.CTX_DEFAULTS)
+    CTX.update(CTX_DEFAULTS)
 
 
 @pytest.fixture(scope="function")
