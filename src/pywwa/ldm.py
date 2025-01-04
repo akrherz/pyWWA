c301e12f1b01a69f269fcf629823f3ad620a0a89
akrherz <akrherz@iastate.edu>
2025-01-04 12:14:50 -0600

    🐛 Wait for database to finish, before shutdown
    fixes #264

--- src/pywwa/ldm.py
+++ src/pywwa/ldm.py
@@ -1,10 +1,11 @@
 """Unidata LDM pqact bridge."""
 
 # 3rd Party
+from twisted.enterprise.adbapi import ConnectionPool
 from twisted.internet import reactor, task
 
 # Local
-from pywwa import LOG, ldmbridge, shutdown
+from pywwa import LOG, ldmbridge
 from pywwa.common import SETTINGS, email_error
 
 
@@ -19,15 +20,21 @@ class MyProductIngestor(ldmbridge.LDMProductReceiver):
 
     def connectionLost(self, reason=None):
         """called when the connection is lost"""
-        LOG.info("connectionLost: %s", reason)
-        shutdown()
+        LOG.info("STDIN Closed: %s, reactor.stop() in 1 second.", reason)
+        reactor.callLater(1, reactor.stop)
 
     def process_data(self, data):
         """Override below."""
         self.local_callback(data)
 
 
-def bridge(callback, dbpool=None, isbinary=False, product_end=None, cb2=None):
+def bridge(
+    callback,
+    dbpool: ConnectionPool = None,
+    isbinary=False,
+    product_end=None,
+    cb2=None,
+):
     """Build a pqact bridge.
 
     Params:
@@ -40,7 +47,7 @@ def bridge(callback, dbpool=None, isbinary=False, product_end=None, cb2=None):
     def dbproxy(data):
         """standardized transaction callback."""
         defer = dbpool.runInteraction(callback, data)
-        if cb2:
+        if cb2 is not None:
             defer.addCallback(cb2)
         defer.addErrback(email_error, data)
         defer.addErrback(LOG.error)
@@ -48,7 +55,7 @@ def bridge(callback, dbpool=None, isbinary=False, product_end=None, cb2=None):
     def nodbproxy(data):
         """Just do a deferred."""
         defer = task.deferLater(reactor, 0, callback, data)
-        if cb2:
+        if cb2 is not None:
             defer.addCallback(cb2)
         defer.addErrback(email_error, data)
         defer.addErrback(LOG.error)
@@ -58,5 +65,25 @@ def bridge(callback, dbpool=None, isbinary=False, product_end=None, cb2=None):
         isbinary=isbinary,
         product_end=product_end,
     )
+
+    # If we have a dbpool, we don't want the reactor to stop until it clears
+    # out the queue
+    if dbpool is not None:
+
+        def _connectionLost(_reason=None):
+            """Shutdown the reactor if we are done."""
+            waiting = dbpool.threadpool._queue.qsize()
+            LOG.info(
+                "STDIN closed. dbpool[dbname=%s] waiting: %s",
+                dbpool.connkw.get("dbname"),
+                waiting,
+            )
+            if waiting == 0:
+                reactor.callLater(1, reactor.stop)
+            else:
+                reactor.callLater(10, _connectionLost, _reason)
+
+        proto.connectionLost = _connectionLost
+
     ldmbridge.LDMProductFactory(proto)
     return proto
