34af501010f27b7d5e886971abeaff96982c233a
akrherz <akrherz@iastate.edu>
2025-01-06 13:40:45 -0600

    🎨 Address lint

--- util/rotate.py
+++ util/rotate.py
@@ -8,16 +8,8 @@ from io import BytesIO
 BASE = "/mesonet/ldmdata/"
 
 
-def main(argv):
-    """Do SOmething"""
-    data = sys.stdin.buffer.read()
-    fnbase = argv[1]
-    fmt = argv[2]
-
-    dirname = f"{BASE}/{os.path.dirname(fnbase)}"
-    if not os.path.isdir(dirname):
-        os.makedirs(dirname)
-
+def handler(fnbase: str, fmt: str, bio: BytesIO):
+    """Do things."""
     if fmt == "tif.Z":
         for i in range(10, -1, -1):
             oldfp = f"{BASE}/{fnbase}{i}.{fmt}"
@@ -25,15 +17,14 @@ def main(argv):
             if os.path.isfile(oldfp):
                 os.rename(oldfp, newfp)
 
-        with BytesIO(data) as bio:
-            # Write out the compressed version verbatim
-            with open(f"{BASE}/{fnbase}0.{fmt}", "wb") as fh:
-                fh.write(bio.getvalue())
+        # Write out the compressed version verbatim
+        with open(f"{BASE}/{fnbase}0.{fmt}", "wb") as fh:
+            fh.write(bio.getvalue())
 
-            # Create the uncompressed version
-            bio.seek(0)
-            with gzip.open(bio, "rb") as fh:
-                data = fh.read()
+        # Create the uncompressed version
+        bio.seek(0)
+        with gzip.open(bio, "rb") as fh:
+            data = fh.read()
         fmt = "tif"
 
     for i in range(10, -1, -1):
@@ -44,6 +35,17 @@ def main(argv):
 
     with open(f"{BASE}/{fnbase}0.{fmt}", "wb") as fh:
         fh.write(data)
+
+
+def main(argv):
+    """Do SOmething"""
+    fnbase = argv[1]
+    fmt = argv[2]
+    dirname = f"{BASE}/{os.path.dirname(fnbase)}"
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+    with BytesIO(sys.stdin.buffer.read()) as bio:
+        handler(fnbase, fmt, bio)
 
 
 if __name__ == "__main__":
